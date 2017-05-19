790ef9396ca448b3c335ea282882dcf7402fc6d0
Magda Skowroń <mskowron0819@gmail.com>
2017-05-19 15:22:52 +0200

    nav-bar for mobile done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menu = document.getElementById('menu');
+    var menu = document.getElementById('hamburger-menu');
+    var nav = document.getElementById('nav-bar');
 
     menu.addEventListener('click', function(e){
         menu.classList.toggle('open');
+        nav.classList.toggle('show');
+
     });
 
     console.log(menu);
