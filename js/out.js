f1eeb75691c1651b6b0e1e80754ae38b75b1b02b
Magda Skowroń <mskowron0819@gmail.com>
2017-05-22 13:36:24 +0200

    change date format in history section

--- js/out.js
+++ js/out.js
@@ -4206,8 +4206,7 @@ $(document).ready(function () {
 
     function insertContent(data) {
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i]);
-            var item = '<div class="row history-item">' + '<div class="col-xs-2 date">' + data[i].date.slice(-5) + '</div>' + '<div class="col-xs-6 description">' + data[i].description + '<div class="row">' + '<div class="col-xs-4 category">' + '<select name="type">' + '<option value=${date[i].category}>' + data[i].category + '</option>' + '<option value="Food">Food</option>' + '<option value="Salary">Salary</option>' + '<option value="Fun">Fun</option>' + '</select>' + '</div>' + '</div>' + ' </div>' + '<div class="col-xs-4 amount"><strong>' + data[i].amount.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '</strong> PLN</div>' + '</div>';
+            var item = '<div class="row history-item">' + '<div class="col-xs-2 date">' + data[i].date.slice(5, 10).split('-').reverse().join('.') + '</div>' + '<div class="col-xs-6 description">' + data[i].description + '<div class="row">' + '<div class="col-xs-4 category">' + '<select name="type">' + '<option value=${date[i].category}>' + data[i].category + '</option>' + '<option value="Food">Food</option>' + '<option value="Salary">Salary</option>' + '<option value="Fun">Fun</option>' + '</select>' + '</div>' + '</div>' + ' </div>' + '<div class="col-xs-4 amount"><strong>' + data[i].amount.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ") + '</strong> PLN</div>' + '</div>';
             $('#history').append(item);
         }
     }
